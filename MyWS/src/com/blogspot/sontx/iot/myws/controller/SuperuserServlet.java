81d7c4f35beebc54b889b18db2a8ebb28075d771
xuanson33bk <xuanson33bk@gmail.com>
2016-01-01 16:35:27 +0700

    + support delete and add new account in SuperuserServlet

--- MyWS/src/com/blogspot/sontx/iot/myws/controller/SuperuserServlet.java
+++ MyWS/src/com/blogspot/sontx/iot/myws/controller/SuperuserServlet.java
@@ -9,7 +9,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.blogspot.sontx.iot.myws.model.bo.SQLMgr;
 import com.blogspot.sontx.iot.myws.utils.Config;
+import com.blogspot.sontx.iot.shared.Security;
+import com.blogspot.sontx.iot.shared.model.bean.Account;
 import com.blogspot.sontx.iot.shared.utils.Convert;
 
 @WebServlet("/SuperuserServlet")
@@ -22,17 +25,51 @@ public class SuperuserServlet extends HttpServlet {
 		return logined;
 	}
 
+	private void checkLogin(HttpServletRequest request) {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		if ((username == null || password == null)
+				|| (!username.equals(Config.SU_USERNAME) || !password.equals(Config.SU_PASSWORD))) {
+			request.getSession().setAttribute("logined", 0);
+		} else {
+			request.getSession().setAttribute("logined", 1);
+		}
+	}
+
+	private void deleteAccount(HttpServletRequest request) {
+		int userid = Convert.parseInt(request.getParameter("id"), -1);
+		SQLMgr.getInstance().deleteAccount(userid);
+	}
+
+	private void createAccount(HttpServletRequest request) {
+		String username = request.getParameter("username");
+		if (Account.checkUserName(username)) {
+			String password = request.getParameter("password");
+			String passwordHash = Security.getPasswordHash(password);
+			if (passwordHash != null && passwordHash.length() > 0) {
+				Account account = new Account();
+				account.setUserName(username);
+				account.setPasswordHash(passwordHash);
+				SQLMgr.getInstance().addAccount(account);
+			}
+		}
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
 		if (!logined(request)) {
-			String username = request.getParameter("username");
-			String password = request.getParameter("password");
-			if ((username == null || password == null)
-				|| (!username.equals(Config.SU_USERNAME) || 
-				!password.equals(Config.SU_PASSWORD))) {
-				request.getSession().setAttribute("logined", 0);
-			} else {
-				request.getSession().setAttribute("logined", 1);
+			checkLogin(request);
+		} else {
+			String req = request.getParameter("req");
+			if (req != null) {
+				switch (req) {
+				case "delete":
+					deleteAccount(request);
+					break;
+				case "new":
+					createAccount(request);
+					break;
+				}
 			}
 		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher("SuperuserPage.jsp");
