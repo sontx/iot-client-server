093f78d4afad3be1126ec3bcf5e50eb584c94494
xuanson33bk <xuanson33bk@gmail.com>
2016-01-01 13:44:08 +0700

    * missing servlet annotation define in EnergyServlet

--- MyWS/src/com/blogspot/sontx/iot/myws/controller/EnergyServlet.java
+++ MyWS/src/com/blogspot/sontx/iot/myws/controller/EnergyServlet.java
@@ -3,12 +3,14 @@ package com.blogspot.sontx.iot.myws.controller;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.blogspot.sontx.iot.myws.model.bo.SQLMgr;
 import com.blogspot.sontx.iot.shared.utils.Convert;
 
+@WebServlet("/EnergyServlet")
 public class EnergyServlet extends BaseServlet {
 	private static final long serialVersionUID = 1L;
 
