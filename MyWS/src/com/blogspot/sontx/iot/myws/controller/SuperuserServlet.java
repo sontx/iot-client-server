0268f980ea854a9b353c0a35183e63e12843014b
xuanson33bk <xuanson33bk@gmail.com>
2016-01-01 15:56:51 +0700

    + check login and save current login session in superuser servlet

--- MyWS/src/com/blogspot/sontx/iot/myws/controller/SuperuserServlet.java
+++ MyWS/src/com/blogspot/sontx/iot/myws/controller/SuperuserServlet.java
@@ -9,12 +9,32 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.blogspot.sontx.iot.myws.utils.Config;
+import com.blogspot.sontx.iot.shared.utils.Convert;
+
 @WebServlet("/SuperuserServlet")
 public class SuperuserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
+	public static boolean logined(HttpServletRequest request) {
+		String s_logined = (String) request.getSession().getAttribute("logined");
+		boolean logined = Convert.parseInt(s_logined, 0) != 0;
+		return logined;
+	}
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
+		if (!logined(request)) {
+			String username = request.getParameter("username");
+			String password = request.getParameter("password");
+			if ((username == null || password == null)
+				|| (!username.equals(Config.SU_USERNAME) || 
+				!password.equals(Config.SU_PASSWORD))) {
+				request.getSession().setAttribute("logined", 0);
+			} else {
+				request.getSession().setAttribute("logined", 1);
+			}
+		}
 		RequestDispatcher dispatcher = request.getRequestDispatcher("SuperuserPage.jsp");
 		dispatcher.forward(request, response);
 	}
