429cce2dbe6d03640c8537b0ce6dadb8a243730e
noem <xuanson33bk@gmail.com>
2016-01-01 16:30:02 +0700

    + support convert bytes array to hex string

--- Shared/src/com/blogspot/sontx/iot/shared/utils/Convert.java
+++ Shared/src/com/blogspot/sontx/iot/shared/utils/Convert.java
@@ -63,6 +63,14 @@ public final class Convert {
 		}
 	}
 
+	public static String bytesToHexString(byte[] b) {
+		String result = "";
+		for (int i = 0; i < b.length; i++) {
+			result += Integer.toString((b[i] & 0xff) + 0x100, 16).substring(1);
+		}
+		return result;
+	}
+
 	private Convert() {
 	}
 
